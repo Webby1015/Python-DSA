6f18623cbb4e80130e07740fd5288ca47b6b2731
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-25 03:32:07 +0530

    single inheritence

---
+++ OOP/inheritence.py
@@ -0,0 +1,30 @@
+class Vehicle:
+    def general_usage(self):
+        print("General usage is to provide Transportation")
+
+
+class Car(Vehicle):
+    def __init__(self):
+        print("I'm Car")
+        self.wheels = 4
+        self.has_roof = True
+
+    def specific_usage(self):
+        print("Specific usage: Commute to work, vacation with family")
+
+
+class MotorCycle(Vehicle):
+    def __init__(self):
+        self.wheels = 2
+        self.roof = False
+
+    def specific_usage(self):
+        print("Specific usage: Road trip, Racing ")
+
+
+c = Car()
+c.general_usage()
+c.specific_usage()
+b = MotorCycle()
+b.general_usage()
+b.specific_usage()
