31cfa385d853a33c446ed6397e58a639a71f1bf4
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-28 19:48:06 +0530

    Raise Exception

---
+++ RaiseException.py
@@ -0,0 +1,15 @@
+# you can google python builtin exceptions to get a list of them
+
+try:
+    raise MemoryError('memory error')
+except MemoryError as e:
+    print(e)
+
+# all the exceptions are raised form a generic class exception
+
+try:
+    raise Exception("Exception")
+except Exception as e:
+    print(e)
+
+# All exceptions are an instance of class
