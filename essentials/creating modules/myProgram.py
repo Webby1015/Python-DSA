16021413fcee24db04ace10cc19c1de6929f395d
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-29 19:31:28 +0530

    creating modules

--- essentials/creating modules/myProgram.py
+++ essentials/creating modules/myProgram.py
@@ -1,4 +1,10 @@
 import functions
 
+# if functions was no in the same directiory
+# we would write foldername.filename.py
+# if the file is somewhere random on system we would write its entire path
+# import sys
+# sys.path.append("c:\Code")
+# now we can import the functions like normal (import functions)
 print(functions.square_area(4))
 print(functions.triangle_area(4, 5))
