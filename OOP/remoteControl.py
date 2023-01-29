7a4ea29d31f388af4ee749c862a4a89c80708999
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-29 17:03:24 +0530

    i dont under stand this error

--- OOP/remoteControl.py
+++ OOP/remoteControl.py
@@ -2,15 +2,18 @@ class RemoteControl():
     def __init_(self):
         self.channels = ["HBO", "CNN", "ABC", "ESPN"]
         self.index = -1
+# here by using __iter__ on self we get the beginning address of that instance
 
     def __iter__(self):
         return self
+# by defining __next__ we change the normal use of __next__ to whatever we defined
 
     def __next__(self):
-         self.index += 1
+        # self.index += 1
         if self.index == len(self.channels):
             raise StopIteration
         return self.channels[self.index]
+
 
 # by writing __int__ , __next__ like this we are calling these methods in our class
 r = RemoteControl()
