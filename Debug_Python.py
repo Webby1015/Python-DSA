1d0678408c315129eec1292aef500bc7b5826569
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-29 16:52:25 +0530

    method and call in class

--- Debug_Python.py
+++ Debug_Python.py
@@ -9,8 +9,20 @@ class User:
     def get_email(self):
         return self._email
 
-    def do_something(self):
-        print("HI FROM ," + str(self))
+# the __str__ method is used to define what happens if we ues str()
+# if we dont declare that method we will get the address of that instance class
 
     def __str__(self):
-        return self.name + ","
+        return self._name + "," + self._email
+# if we had not declared str it just would have changed the address as string value and printed that
+
+    def do_something(self):
+        print("HI FROM ,"+str(self))
+
+
+users = [User("Testuser", "testmail@gmail.com"),
+         User("User2", "user2@gmail.com")]
+print()
+for user in users:
+    user.do_something()
+print()
