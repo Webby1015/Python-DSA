4deac919d509317ba8c74cf90f3f3c2fa681c733
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-29 18:56:57 +0530

    round method

---
+++ round.py
@@ -0,0 +1,6 @@
+# rounds up floats to some decimal points
+a = 1.234556789
+print(a)
+print(round(a, 2))
+print(round(a))
+# if no. of decimal points are not specified it increases by one if > .5 else prints just the interger value
