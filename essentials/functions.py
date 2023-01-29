fcf59153d8309475073523482e7acc6abf5d1363
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-01-29 19:07:11 +0530

    functions

---
+++ essentials/functions.py
@@ -0,0 +1,11 @@
+def add(a, b):
+
+    return a+b
+
+# two types of calling an functions with arguments
+
+
+# in first one we write the values in order in which the arguments were declared
+print(add(4, 5))
+# in second we write the values with their argument names this is helpful when theres a long list of arguments
+print(add(b=5, a=4))
