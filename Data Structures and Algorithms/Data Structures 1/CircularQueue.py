e518051d95a196130498dd3b36ceee431c54461c
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-11 14:39:12 +0530

    ..

--- Data Structures and Algorithms/Data Structures 1/CircularQueue.py
+++ Data Structures and Algorithms/Data Structures 1/CircularQueue.py
@@ -1,42 +1,9 @@
 class Circularqueue():
     def __init__(self, k):
+        self.k = k
         self.queue = [None] * k
         self.head = self.tail = -1
 
-    def enqueue(self, item):
-        if ((self.tail + 1) % self.k == self.head):
-            print("Queue is full")
-        elif (self.head == -1):
-            self.head = 0
-            self.tail = 0
-            self.queue[self.tail] = item
-        else:
-            self.tail == (self.tail + 1) % self.k
-            self.queue[self.tail] = data
-
     def dequeue(self):
-        if(self.head == -1):
-            print("queue is empty")
-        elif(self.head == self.tail):
-            temp = self.queue[self.head]
-            self.head = -1
-            self.tail = -1
-            return temp
-        else:
-            temp = self.queue[self.head]
-            self.head = (self.head + 1) % self.k
-            return temp
-
-        def printCqueue(self):
-            if(self.head == -1):
-                print("Empty queue")
-            elif (self.tail >= self.head):
-                for i in range(self.head, self.tail+1):
-                    print(self.queue[i], end=" ")
-                print()
-            else:
-                for i in range(self.head, self.k):
-                    print(self.queue[i], end=" ")
-                for i in range(0, self.tail+1):
-                    print(self.queue[i], end=" ")
-                print()
+        if self.head == self.tail:
+            print("queue empty")
